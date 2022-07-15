b17130fb244bef4f01e8b8ff839a852afe38fa9e
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-07-15 06:47:46 -0400

    Add files via upload

---
+++ file4/ifstatement.js
@@ -0,0 +1,25 @@
+//if statement  -  a basic form of decision making
+//                 if a condition is true,then do something
+//                 if not,then don't do it.
+/*let age = 65;
+
+if(age >= 18){
+    console.log("You are an adult");
+}
+else if(age < 0){
+    console.log("You haven't been born YET!");
+}
+else if(age > 65){
+    console.log("You are a senior citizen");
+}
+else{
+    console.log("You are a child");
+}*/
+let online = true;
+
+if (online){
+    console.log("You are online");
+}
+else{
+    console.log("You are offline!");
+}
---
+++ file4/index.js
@@ -0,0 +1,9 @@
+//method chaining  =  calling one method after another
+//                    in one continous line of code
+//It makes the code cleaner and more readable
+let userName = "bro";
+
+let letter = userName.charAt(0).toUpperCase().trim();
+
+
+console.log(letter);
